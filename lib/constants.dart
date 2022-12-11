24aa32b65331e005932ec95b284570efa92572ad
Md. Mehedi Hasan <mdmehedi.oz00@gmail.com>
2022-12-11 07:14:04 +0600

    Files Updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'size_config.dart';
+import 'package:shop_app/size_config.dart';
 
 const kPrimaryColor = Color(0xFFFF7643);
 const kPrimaryLightColor = Color(0xFFFFECDF);
@@ -14,36 +14,37 @@ const kTextColor = Color(0xFF757575);
 const kAnimationDuration = Duration(milliseconds: 200);
 
 final headingStyle = TextStyle(
-  fontSize: getProportionateScreenHeight(28),
+  fontSize: getProportionateScreenWidth(28),
   fontWeight: FontWeight.bold,
   color: Colors.black,
   height: 1.5,
 );
 
+const defaultDuration = Duration(milliseconds: 250);
+
 // Form Error
 final RegExp emailValidatorRegExp =
-    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]+");
-const String kEmailNullError = "Please enter your Email";
-const String kInvalidEmailError = "Enter a valid Email";
-const String kPassNullError = "Please enter your Password";
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
 const String kShortPassError = "Password is too short";
-const String kMatchPassError = "PAssword don't match";
-// ignore: constant_identifier_names
-const String KNamelNullError = "Please Enter Your Name";
-// ignore: constant_identifier_names
-const String KPhoneNumberNullError = "Please Enter Your Phone Number";
-const String KAddressNullError = "Please Enter Your Address";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
 
 final otpInputDecoration = InputDecoration(
   contentPadding:
       EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
-  enabledBorder: outlineInputBorder(),
-  focusedBorder: outlineInputBorder(),
   border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
 );
 
 OutlineInputBorder outlineInputBorder() {
   return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(15),
-      borderSide: BorderSide(color: kTextColor));
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
 }
