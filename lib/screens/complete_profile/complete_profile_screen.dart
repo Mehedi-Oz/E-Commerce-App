85550739ef119a283d43406eba7b9e8b02711295
Md. Mehedi Hasan <mdmehedi.oz00@gmail.com>
2022-12-11 07:29:05 +0600

    Sign Up

--- lib/screens/complete_profile/complete_profile_screen.dart
+++ lib/screens/complete_profile/complete_profile_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/screens/complete_profile/components/body.dart';
+
+import 'components/body.dart';
 
 class CompleteProfileScreen extends StatelessWidget {
-  const CompleteProfileScreen({super.key});
   static String routeName = "/complete_profile";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign Up"),
+        title: Text('Sign Up'),
       ),
       body: Body(),
     );
--- lib/screens/complete_profile/components/complete_profile_form.dart
+++ lib/screens/complete_profile/components/complete_profile_form.dart
@@ -1,59 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/screens/otp/otp_screen.dart';
+import 'package:shop_app/components/custom_surfix_icon.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/components/form_error.dart';
+import 'package:shop_app/screens/otp/otp_screen.dart';
 
-import '../../../components/custom_sufix_icon.dart';
-import '../../../components/default_button.dart';
 import '../../../constants.dart';
 import '../../../size_config.dart';
 
 class CompleteProfileForm extends StatefulWidget {
-  const CompleteProfileForm({super.key});
-
   @override
-  State<CompleteProfileForm> createState() => _CompleteProfileFormState();
+  _CompleteProfileFormState createState() => _CompleteProfileFormState();
 }
 
 class _CompleteProfileFormState extends State<CompleteProfileForm> {
   final _formKey = GlobalKey<FormState>();
-  late String firstName;
-  late String lastName;
-  late String phoneNumber;
-  late String address;
+  final List<String?> errors = [];
+  String? firstName;
+  String? lastName;
+  String? phoneNumber;
+  String? address;
 
-  get errors => null;
+  void addError({String? error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
         children: [
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
-          buildFirstNameFieldForm(),
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
-          buildLastNameFieldForm(),
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
-          buildPhoneNumberFromField(),
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
+          buildFirstNameFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildLastNameFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPhoneNumberFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
           buildAddressFormField(),
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(40)),
           DefaultButton(
-              text: "Continue",
-              press: () {
-                if (_formKey.currentState!.validate()) {
-                  //go to OTP Screen
-                  Navigator.pushNamed(context, OtpScreen.routeName);
-                }
-              })
+            text: "continue",
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                Navigator.pushNamed(context, OtpScreen.routeName);
+              }
+            },
+          ),
         ],
       ),
     );
@@ -61,109 +64,97 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
 
   TextFormField buildAddressFormField() {
     return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (newValue) => address = newValue!,
+      onSaved: (newValue) => address = newValue,
       onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(KAddressNullError)) {
-          setState(() {
-            errors.remove(KAddressNullError);
-          });
+        if (value.isNotEmpty) {
+          removeError(error: kAddressNullError);
         }
         return null;
       },
-      validator: ((value) {
-        if (value!.isEmpty && errors.contains(KAddressNullError)) {
-          setState(() {
-            errors.add(KAddressNullError);
-          });
-          // return "";
-        }
-        return null;
-      }),
-      decoration: InputDecoration(
-        labelText: "Address",
-        hintText: "Enter your Address",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/Location point.svg",
-        ),
-      ),
-    );
-  }
-
-  TextFormField buildPhoneNumberFromField() {
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      onSaved: (newValue) => phoneNumber = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(KPhoneNumberNullError)) {
-          setState(() {
-            errors.remove(KPhoneNumberNullError);
-          });
-        }
-        return null;
-      },
-      validator: ((value) {
-        if (value!.isEmpty && errors.contains(KPhoneNumberNullError)) {
-          setState(() {
-            errors.add(KPhoneNumberNullError);
-          });
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kAddressNullError);
           return "";
         }
         return null;
-      }),
+      },
       decoration: InputDecoration(
-        labelText: "Phone Number",
-        hintText: "Enter your Phone Number",
+        labelText: "Address",
+        hintText: "Enter your phone address",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
         floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/Phone.svg",
-        ),
+        suffixIcon:
+            CustomSurffixIcon(svgIcon: "assets/icons/Location point.svg"),
       ),
     );
   }
 
-  TextFormField buildLastNameFieldForm() {
+  TextFormField buildPhoneNumberFormField() {
     return TextFormField(
-      onSaved: (newValue) => lastName = newValue!,
-      decoration: InputDecoration(
-        labelText: "Last Name",
-        hintText: "Enter your Last Name",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/User.svg",
-        ),
-      ),
-    );
-  }
-
-  TextFormField buildFirstNameFieldForm() {
-    return TextFormField(
-      onSaved: (newValue) => firstName = newValue!,
+      keyboardType: TextInputType.phone,
+      onSaved: (newValue) => phoneNumber = newValue,
       onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(KNamelNullError);
-          });
+        if (value.isNotEmpty) {
+          removeError(error: kPhoneNumberNullError);
         }
         return null;
       },
-      validator: ((value) {
-        if (value!.isEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.add(KNamelNullError);
-          });
-          // return "";
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kPhoneNumberNullError);
+          return "";
         }
         return null;
-      }),
+      },
+      decoration: InputDecoration(
+        labelText: "Phone Number",
+        hintText: "Enter your phone number",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Phone.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildLastNameFormField() {
+    return TextFormField(
+      onSaved: (newValue) => lastName = newValue,
+      decoration: InputDecoration(
+        labelText: "Last Name",
+        hintText: "Enter your last name",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/User.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildFirstNameFormField() {
+    return TextFormField(
+      onSaved: (newValue) => firstName = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kNamelNullError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kNamelNullError);
+          return "";
+        }
+        return null;
+      },
       decoration: InputDecoration(
         labelText: "First Name",
-        hintText: "Enter your First Name",
+        hintText: "Enter your first name",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
         floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/User.svg",
-        ),
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/User.svg"),
       ),
     );
   }
